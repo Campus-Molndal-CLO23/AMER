9dd564ce833364dd74934e877f4494f2e0ce674d
amer <uzakha@gmail.com>
2023-09-12 13:33:15 +0200

    new project

---
+++ Lösenordskontroll/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+string korrektLosenord = "MittLosenord123";
+
+Console.WriteLine("Mata in din Losenord....   ");
+string anvanadreLosenord = Console.ReadLine();
+
+if (korrektLosenord == anvanadreLosenord)
+{
+    Console.WriteLine("Välkommen, Jedi-mästare!");
+}
+else
+{
+    Console.WriteLine("Fel lösenord. Försök igen.");
+
+}
--- pro2/Program.cs
+++ pro2/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+        double feh = 25.7;
+        double celcius = (feh * 9 / 5) + 32;
+
+
+
+        //Console.WriteLine("Temperatue in Celcius is....." + celcius);
+        Console.WriteLine("Temperatue in Celcius is {0} ", celcius);
+    }
+}
