858bc7423f33c578eedbea586727d8956cdd8587
amer <uzakha@gmail.com>
2023-09-12 13:54:34 +0200

    Ålder kontroll

---
+++ Ålderskontroll/Program.cs
@@ -0,0 +1,17 @@
+﻿// Ålder Kontroll
+
+
+int minAlder = 18;
+
+Console.WriteLine("Hur Gammal är du?");
+int användarenAlder = Convert.ToInt32(Console.ReadLine());
+
+if(användarenAlder >= minAlder)
+{
+    Console.WriteLine("Du är gammal nog att köra rymdskepp!");
+
+}
+else
+{
+    Console.WriteLine("Du är för ung för att köra rymdskepp..");
+}
